a16bea3ab9215651e7c7b62d4fea0de32266e1fd
Contreras Perales Rodrigo <levicom134@gmail.com>
2024-11-13 15:00:36 -0500

    Create navbar in component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import { Routes, Route } from 'react-router-dom'
-import Navbar from "./pages/Navbar"
-import Home from './pages/Home'
-import Categoria from './pages/Categoria'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Buscar from './pages/Buscar'
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'; // Importa el nuevo componente de Navbar
+import Home from './pages/Home';
+import Categoria from './pages/Categoria';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Buscar from './pages/Buscar';
 
 export const App = () => {
   return (
     <>
+      <Navbar /> {/* Muestra el Navbar en todas las páginas */}
       <Routes>
-        <Route path="/" element={<Navbar/>} >
-          <Route index element={<Home />} />
-          <Route path='categoria' element={<Categoria />} />
-          <Route path='buscar' element={<Buscar />} />
-          <Route path='login' element={<Login />} />
-          <Route path='register' element={<Register />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="categoria" element={<Categoria />} />
+        <Route path="buscar" element={<Buscar />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Routes>
     </>
-  )
+  );
 }
-
